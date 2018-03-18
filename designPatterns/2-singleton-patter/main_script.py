0ce988465ae3c2d9955bc039b6e1c6e71a564a36
Raúl e. López Briega <relopezbriega@gmail.com>
2018-03-18 15:12:55 -0300

    singleton pattern

--- designPatterns/2-singleton-patter/main_script.py
+++
@@ -1,4 +0,0 @@
-import logger
-
-for i in range(4):
-    logger.log_message("Log message {}".format(i))
--- designPatterns/2-singleton-patter/test_error_log.py
+++
@@ -1,6 +0,0 @@
-import logger
-
-try:
-    a = 1 / 0
-except:
-    logger.error("something went wrong")
